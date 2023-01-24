f9f3fb3fb3621ab0160d38ae3bd1990532ae6e7e
Rustam Kadimov <kadim_r@mail.ru>
2023-01-24 19:13:40 +0500

    sd

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+package main.java;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+//        CrptApi crptApi = new CrptApi(TimeUnit.SECONDS, 1);
+//
+//        CrptApi.Document document = crptApi.new Document("12345", "string", "string", "12345",
+//                "1234", new Date(), "type", "4321");
+//        CrptApi.Product product = crptApi.new Product(new Date(), "123323", "dsfdf", "");
+//        document.addProduct(product);
+//
+//        for (int i = 0; i < 10; i++) {
+//            //Thread.sleep(500);
+//            crptApi.createDocument(document, "podpis");
+//        }
+//
+//        System.out.println(crptApi.count);
+
+    }
+}
